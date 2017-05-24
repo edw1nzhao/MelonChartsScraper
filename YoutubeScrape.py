9b38a8b50b475416ff915addcfb4f9f57c26498a
Edwin Zhao <edwin.zhao@outlook.com>
2017-05-24 21:43:58 +0800

    Completed get method for top 100 links

--- YoutubeScrape.py
+++ YoutubeScrape.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-url = 'https://www.youtube.com/watch?v=vcXQb3SdpVs&list=PLBSvleni5is2u66UMpc7nLOK6hh7w5OrQ&index=1'
+top100 = []
+melonUrl = 'https://www.youtube.com/watch?v=vcXQb3SdpVs&list=PLBSvleni5is2u66UMpc7nLOK6hh7w5OrQ&index=1'
 
-r = requests.get(url)
-page = r.text
-soup = bs(page,'html.parser')
-res = soup.find_all('a', {'class':'playlist-video'})
-print(res)
-# for x in res:
-#     print x.get("href")
+
+def getTop100():
+    r = requests.get(melonUrl)
+    page = r.text
+    soup = bs(page,'html.parser')
+    res = soup.find_all('a', {'class':'playlist-video'})
+
+    count = 0
+    for x in res:
+        link = x.get("href")
+        if "/watch" in link and count != 0 and count != 101:
+            top100.append(link)
+        count += 1
