735e892ac8b123cd72468b459bcf54fd924026e1
Edwin Zhao <edwin.zhao@outlook.com>
2017-06-03 18:45:09 +0800

    Proper adding and sorting of duplicates

--- YoutubeScrape.py
+++ YoutubeScrape.py
@@ -1,15 +1,40 @@
 from bs4 import BeautifulSoup as bs
 from twilio.rest import Client
-import requests
 
+import os
+import shutil
+import requests
+import youtube_dl
+
+
+######################################################
+######################################################
+
+noFile = True
 authCode = '01892c169626b397acca0b7f1d68dbc3'
 accSID = 'AC9f95cae35e07f7b38ea9069c6f74c39a'
-newList = []
-loadedList = []
+tempSongs = []
+newSongs = []
+loadedSongs = []
+
 youtubeUrl = 'https://www.youtube.com'
 melonUrl = youtubeUrl + '/watch?v=vcXQb3SdpVs&list=PLBSvleni5is2u66UMpc7nLOK6hh7w5OrQ&index=1'
 fileName = 'downloadedSongs.txt'
 
+sourcePath = '/Users/voltageShift/repository/MelonChartsScraper/'
+source = os.listdir(sourcePath)
+destPath = '/Users/voltageShift/repository/MelonChartsScraper/Downloaded'
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'verbose': True,
+    'noplaylist':True,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 ######################################################
 ######################################################
 
@@ -18,41 +43,22 @@ def loadTwilio():
     client = Client(accSID, authCode)
 
 def loadList():
-    global loadedList
+    global loadedSongs
+    global noFile
+
     try:
         with open(fileName, 'r') as f:
-            loadedList = [line[:-1] for line in f]
-            print('There were previously: ' + str(len(loadedList)) \
+            loadedSongs = [line[:-1].strip() for line in f]
+            if len(loadedSongs) is not 0:
+                noFile = False
+            print('There were previously: ' + str(len(loadedSongs)) \
                 + ' songs saved.')
     except Exception as error:
         print('There was no previous file saved.')
 
-def sendText(num):
-    global client
-    if num is not 0:
-        client.messages.create(to="+13522224358",
-                               from_="+8613162355703",
-                               body="There were " + str(num) + " new songs.")
+def getnewSongs():
+    global newSongs
 
-def writeList():
-    global newList
-
-    with open(fileName, 'a') as f:
-        for x in newList:
-            f.write(x)
-            f.write('\n')
-
-def updateList():
-    global loadedList
-    global newList
-
-    loadList()
-
-    num = len(newList)
-    sendText(num)
-    writeList()
-
-def getNewList():
     r = requests.get(melonUrl)
     page = r.text
     soup = bs(page,'html.parser')
@@ -62,12 +68,62 @@ def getNewList():
     for x in res:
         link = x.get('href')
         if '/watch' in link and count != 0 and count != 101:
-            newList.append(link)
+            newSongs.append(link.strip())
         count += 1
 
+def updateList():
+    global loadedSongs
+    global newSongs
+    global tempSongs
+    global noFile
+    # print("prev load" + str(len(loadedSongs)))
+    # print("prev news" + str(len(newSongs)))
+    # print("prev temp" + str(len(tempSongs)))
+
+    for i in newSongs:
+        # print(i in loadedSongs);
+        if i not in loadedSongs:
+            tempSongs.append(i)
+
+    # print("post load" + str(len(loadedSongs)))
+    # print("post news" + str(len(newSongs)))
+    # print("post temp" + str(len(tempSongs)))
+
+    writeList()
+
+def writeList():
+    global tempSongs
+    global noFile
+    # print(str(len(tempSongs)) + "hi")
+    with open(fileName, 'a') as f:
+        for x in tempSongs:
+            f.write(x)
+            f.write('\n')
+
+def downloadSongs():
+    global newSongs
+    global source
+
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+    with ydl:
+        for x in newSongs:
+            ydl.download([youtubeUrl + x])
+
+    for files in source:
+        if files.endswith('.mp3'):
+            shutil.move(os.path.join(sourcePath,files), os.path.join(destPath,files))
+
+def sendText(num):
+    global client
+    if num is not 0:
+        client.messages.create(to="+13522224358",
+                               from_="+8613162355703",
+                               body="There are " + str(num) + " new songs added.")
 ######################################################
 ######################################################
 
 loadTwilio()
-getNewList()
+loadList()
+getnewSongs()
 updateList()
+#downloadSongs()
